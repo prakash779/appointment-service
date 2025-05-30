3473e8d17829a97c659a81c1f89427a449099cda
prakash779 <105863754+prakash779@users.noreply.github.com>
2025-05-30 15:25:51 +0530

    region change main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
   name = "main-vpc"
   cidr = "10.0.0.0/16"
-  azs             = ["${var.region}a", "${var.region}b"]
+  azs             = ["eu-north-1a", "eu-north-1b"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   enable_dns_hostnames = true
 }
