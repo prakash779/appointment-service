671bb2449b7dbac14f3f059135d63245a5e6d3bd
prakash779 <105863754+prakash779@users.noreply.github.com>
2025-05-31 03:32:52 +0530

    added terraform

---
+++ terraform/main.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+  region = var.region
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  name = "main-vpc"
+  cidr = "10.0.0.0/16"
+  azs             = ["${var.region}a", "${var.region}b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  enable_dns_hostnames = true
+}
+
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = "eks-cluster"
+  cluster_version = "1.27"
+  subnets         = module.vpc.public_subnets
+  vpc_id          = module.vpc.vpc_id
+
+  node_groups = {
+    eks_nodes = {
+      desired_capacity = 2
+      max_capacity     = 2
+      min_capacity     = 1
+      instance_type    = "t3.medium"
+    }
+  }
+}
+
+resource "aws_ecr_repository" "app" {
+  name = var.module_name
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  description = "AWS Region"
+  default     = "eu-north-1"
+}
+
+variable "module_name" {
+  description = "ECR Repo / App Name"
+  default     = "appointment-service"
+}
